48a16ab4623c5e2c61b140157bbd9bba88152402
ryanfp <71905151+ryanfp@users.noreply.github.com>
2026-02-18 02:35:38 -0500

    Hopefully this fixes the issue with the site, had to reinstall node dependencies and delete mentions of AAG in some places

--- node/build-sw.mjs
+++ node/build-sw.mjs
@@ -73,7 +73,7 @@ const workboxRuntimeBuildResult = await injectManifest({
 	*/
 	globPatterns: [
 		"data/adventure/**/*.json", // matches all adventure json
-		"img/**/*", // matches all images
+		// ".", // matches all images
 		"icon/*.png", // all icons
 		"*.png", // root images
 		"*.svg", // root svg
